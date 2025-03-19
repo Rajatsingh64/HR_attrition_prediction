c967653993348c6d4d9575fc6d825a37ca935fef
RJSINGH64 <rajat.k.singh64@gmail.com>
2025-03-19 12:45:27 +0530

    full deployment CI CD and CT using airflow successfull

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -25,28 +25,25 @@ def start_training_pipeline():
         data_ingestion = DataIngestion(data_ingestion_config)
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
         print(f'{"="*20} Data Ingestion Completed Successfully {"="*20}')
-        logging.info(f'{"="*20} Data Ingestion Completed Successfully {"="*20}')
-
+       
         # Data Validation
         data_validation_config = DataValidationConfig(training_pipeline_config)
         data_validation = DataValidation(data_validation_config, data_ingestion_artifact)
         data_validation_artifact = data_validation.initiate_data_validation()
         print(f'{"="*20} Data Validation Completed Successfully {"="*20}')
-        logging.info(f'{"="*20} Data Validation Completed Successfully {"="*20}')
-
+       
         # Data Transformation
         data_transformation_config = DataTransformationConfig(training_pipeline_config)
         data_transformation = DataTransformation(data_transformation_config, data_ingestion_artifact)
         data_transformation_artifact = data_transformation.initiate_data_transformation()
         print(f'{"="*20} Data Transformation Completed Successfully {"="*20}')
-        logging.info(f'{"="*20} Data Transformation Completed Successfully {"="*20}')
-        
+       
         # Model Training
         model_trainer_config = ModelTrainerConfig(training_pipeline_config)
         model_trainer = ModelTrainer(model_trainer_config, data_transformation_artifact)
         model_trainer_artifact = model_trainer.initiate_model_trainer()
         print(f'{"="*20} Model Training Completed Successfully {"="*20}')
-        logging.info(f'{"="*20} Model Training Completed Successfully {"="*20}')
+       
 
         # Model Evaluation
         model_evaluation_config = config_entity.ModelEvaluationConfig(training_pipeline_config)
@@ -58,14 +55,12 @@ def start_training_pipeline():
         )
         model_evaluation_artifact = model_evaluation.initiate_model_evaluation()
         print(f'{"="*20} Model Evaluation Completed Successfully {"="*20}')
-        logging.info(f'{"="*20} Model Evaluation Completed Successfully {"="*20}')
-
+      
         # Model Pusher
         model_pusher_config = config_entity.ModelPusherConfig(training_pipeline_config)
         model_pusher = ModelPusher(model_pusher_config, data_transformation_artifact, model_trainer_artifact)
         model_pusher_artifact = model_pusher.initiate_model_pusher()
         print(f'{"="*20} Model Pusher Completed Successfully {"="*20}')
-        logging.info(f'{"="*20} Model Pusher Completed Successfully {"="*20}')
-
+       
     except Exception as e:
         raise ProjectException(e, sys)
